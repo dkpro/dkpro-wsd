1153a9ed9ad0aef31ecf1a378f5b3d1a3604d4fe
tristan.miller@nothingisreal.com <tristan.miller@nothingisreal.com@6a9cb421-24ca-3a8e-1600-1ea4fc5865ef>
2013-10-02 12:30:30 +0000

    Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0

--- trunk/de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/AidaReader.java
+++ trunk/de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/AidaReader.java
@@ -36,8 +36,8 @@ import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
-import org.uimafit.component.CasCollectionReader_ImplBase;
-import org.uimafit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.component.CasCollectionReader_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
 import de.tudarmstadt.ukp.dkpro.wsd.type.Sense;
