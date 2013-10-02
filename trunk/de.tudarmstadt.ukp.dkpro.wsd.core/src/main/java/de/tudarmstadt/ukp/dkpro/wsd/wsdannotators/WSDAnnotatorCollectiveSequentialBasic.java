fc3478543845c2d3f30cb937a47b3a88e7054405
tristan.miller@nothingisreal.com <tristan.miller@nothingisreal.com@6a9cb421-24ca-3a8e-1600-1ea4fc5865ef>
2013-10-02 14:00:23 +0000

    Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0

--- trunk/de.tudarmstadt.ukp.dkpro.wsd.core/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/wsdannotators/WSDAnnotatorCollectiveSequentialBasic.java
+++ trunk/de.tudarmstadt.ukp.dkpro.wsd.core/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/wsdannotators/WSDAnnotatorCollectiveSequentialBasic.java
@@ -31,9 +31,9 @@ import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.uimafit.descriptor.ConfigurationParameter;
-import org.uimafit.descriptor.ExternalResource;
-import org.uimafit.util.JCasUtil;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.ExternalResource;
+import org.apache.uima.fit.util.JCasUtil;
 
 import de.tudarmstadt.ukp.dkpro.wsd.algorithms.WSDAlgorithmCollectiveSequentialBasic;
 import de.tudarmstadt.ukp.dkpro.wsd.si.SenseInventoryException;
