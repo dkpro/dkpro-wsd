87e9b0c805c2bbfa334d306d5b82b0e86e2b5c7a
Tristan Miller <psychonaut@nothingisreal.com>
2013-10-02 14:06:21 +0000

    Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0

--- de.tudarmstadt.ukp.dkpro.wsd.evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/evaluation/AbstractClusterEvaluator.java
+++ de.tudarmstadt.ukp.dkpro.wsd.evaluation/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/evaluation/AbstractClusterEvaluator.java
@@ -34,9 +34,9 @@ import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
-import org.uimafit.descriptor.ConfigurationParameter;
-import org.uimafit.descriptor.ExternalResource;
-import org.uimafit.util.JCasUtil;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.ExternalResource;
+import org.apache.uima.fit.util.JCasUtil;
 
 import de.tudarmstadt.ukp.dkpro.wsd.candidates.SenseClusterer;
 import de.tudarmstadt.ukp.dkpro.wsd.si.POS;
