510dd087fc467c0eea464fa0091f7566d1675219
Tristan Miller <psychonaut@nothingisreal.com>
2013-10-02 12:28:31 +0000

    Upgrade to DKPro Core 1.5.0 and uimaFIT 2.0.0

--- de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/writer/WSDWriter.java
+++ de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/writer/WSDWriter.java
@@ -21,8 +21,8 @@ package de.tudarmstadt.ukp.dkpro.wsd.io.writer;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
-import org.uimafit.component.JCasConsumer_ImplBase;
-import org.uimafit.util.JCasUtil;
+import org.apache.uima.fit.component.JCasConsumer_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
 
 import de.tudarmstadt.ukp.dkpro.wsd.type.Sense;
 import de.tudarmstadt.ukp.dkpro.wsd.type.WSDItem;
