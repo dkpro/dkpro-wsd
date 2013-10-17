27a9ef60e9484b15dbdcac84949e04dce1c8394a
Tristan Miller <psychonaut@nothingisreal.com>
2013-10-17 16:42:58 +0000

    Added logging output to SensevalAnswerKeyReader

--- de.tudarmstadt.ukp.dkpro.wsd.senseval/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/SensevalAnswerKeyReader.java
+++ de.tudarmstadt.ukp.dkpro.wsd.senseval/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/SensevalAnswerKeyReader.java
@@ -29,14 +29,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.cas.FSArray;
-import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import de.tudarmstadt.ukp.dkpro.core.api.resources.ResourceUtils;
 import de.tudarmstadt.ukp.dkpro.wsd.type.Sense;
@@ -44,99 +45,153 @@ import de.tudarmstadt.ukp.dkpro.wsd.type.WSDItem;
 import de.tudarmstadt.ukp.dkpro.wsd.type.WSDResult;
 
 /**
- * SensevalAnswerKeyReader reads in a Senseval answer key file and
- * creates a WSDresult annotation for each answer set contained therein.
- * The answer key file format is as follows:
+ * SensevalAnswerKeyReader reads in a Senseval answer key file and creates a
+ * WSDresult annotation for each answer set contained therein. The answer key
+ * file format is as follows:
  *
- * <p>&lt;lexical_sample_answer&gt; ::= &lt;lexelt&gt; &lt;instance_id&gt; &lt;sense_tag_list&gt; [!! &lt;comment&gt;]</p>
- * <p>&lt;all_words_answer&gt; ::= &lt;text_id&gt; &lt;head_id&gt; &lt;sense_tag_list&gt; [!! &lt;comment&gt;]</p>
- * <p>&lt;lexelt&gt; ::= lexical element (as defined in the lex-sample evaluation data)</p>
- * <p>&lt;instance_id&gt; ::= instance id (as defined in the lex-sample evaluation data)</p>
- * <p>&lt;text_id&gt; ::= text id (as defined in the all-words evaluation data)</p>
- * <p>&lt;head_id&gt; ::= head id (as defined in the all-words evaluation data)</p>
- * <p>&lt;sense_tag_list&gt; ::= &lt;weighted_list&gt; | &lt;unweighted_list&gt;</p>
- * <p>&lt;weighted_list&gt; ::= &lt;senseid&gt;[/&lt;weight&gt;] {&lt;senseid&gt;[/&lt;weight&gt;]}</p>
- * <p>&lt;unweighted_list&gt; ::= &lt;senseid&gt; {&lt;senseid&gt;}</p>
- * <p>&lt;senseid&gt; ::= senseid from the lexicon for the task, with apostrophes replaced by hyphens</p>
- * <p>&lt;weight&gt; ::= integer or positive real number</p>
- * <p>&lt;comment&gt; ::= text string</p>
+ * <p>
+ * &lt;lexical_sample_answer&gt; ::= &lt;lexelt&gt; &lt;instance_id&gt;
+ * &lt;sense_tag_list&gt; [!! &lt;comment&gt;]
+ * </p>
+ * <p>
+ * &lt;all_words_answer&gt; ::= &lt;text_id&gt; &lt;head_id&gt;
+ * &lt;sense_tag_list&gt; [!! &lt;comment&gt;]
+ * </p>
+ * <p>
+ * &lt;lexelt&gt; ::= lexical element (as defined in the lex-sample evaluation
+ * data)
+ * </p>
+ * <p>
+ * &lt;instance_id&gt; ::= instance id (as defined in the lex-sample evaluation
+ * data)
+ * </p>
+ * <p>
+ * &lt;text_id&gt; ::= text id (as defined in the all-words evaluation data)
+ * </p>
+ * <p>
+ * &lt;head_id&gt; ::= head id (as defined in the all-words evaluation data)
+ * </p>
+ * <p>
+ * &lt;sense_tag_list&gt; ::= &lt;weighted_list&gt; | &lt;unweighted_list&gt;
+ * </p>
+ * <p>
+ * &lt;weighted_list&gt; ::= &lt;senseid&gt;[/&lt;weight&gt;]
+ * {&lt;senseid&gt;[/&lt;weight&gt;]}
+ * </p>
+ * <p>
+ * &lt;unweighted_list&gt; ::= &lt;senseid&gt; {&lt;senseid&gt;}
+ * </p>
+ * <p>
+ * &lt;senseid&gt; ::= senseid from the lexicon for the task, with apostrophes
+ * replaced by hyphens
+ * </p>
+ * <p>
+ * &lt;weight&gt; ::= integer or positive real number
+ * </p>
+ * <p>
+ * &lt;comment&gt; ::= text string
+ * </p>
  *
- * @author	Tristan Miller <miller@ukp.informatik.tu-darmstadt.de>
+ * @author Tristan Miller <miller@ukp.informatik.tu-darmstadt.de>
  */
-public class SensevalAnswerKeyReader extends JCasAnnotator_ImplBase
+public class SensevalAnswerKeyReader
+    extends JCasAnnotator_ImplBase
 {
+    private final static Logger logger = Logger
+            .getLogger(SensevalAnswerKeyReader.class.getName());
 
-	public static final String PARAM_FILE = "fileName";
-	@ConfigurationParameter(name=PARAM_FILE, mandatory=true, description="The Semeval/Senseval answer key to read")
-	private String fileName;
+    public static final String PARAM_FILE = "fileName";
+    @ConfigurationParameter(name = PARAM_FILE, mandatory = true, description = "The Semeval/Senseval answer key to read")
+    private String fileName;
 
-	public static final String PARAM_SENSE_INVENTORY = "senseInventory";
-	@ConfigurationParameter(name=PARAM_SENSE_INVENTORY, mandatory=false, description="The sense inventory used by the answer key", defaultValue="Senseval_sensekey")
-	private String senseInventory;
+    public static final String PARAM_SENSE_INVENTORY = "senseInventory";
+    @ConfigurationParameter(name = PARAM_SENSE_INVENTORY, mandatory = false, description = "The sense inventory used by the answer key", defaultValue = "Senseval_sensekey")
+    private String senseInventory;
 
-	private Map<String,String> answerKey;
+    private Map<String, String> answerKey;
 
     /**
-	 * @param args
-	 * @throws IOException
-	 */
-	@Override
-	public void initialize(UimaContext context)
-		throws ResourceInitializationException
-	{
-		super.initialize(context);
+     * @param args
+     * @throws IOException
+     */
+    @Override
+    public void initialize(UimaContext context)
+        throws ResourceInitializationException
+    {
+        super.initialize(context);
 
-		answerKey = new HashMap<String, String>();
+        answerKey = new HashMap<String, String>();
 
-		// Read the answer key file into a collection indexed by instance/head ID
+        // Read the answer key file into a collection indexed by instance/head
+        // ID
 
-		BufferedReader bufRead = null;
-		InputStream is = null;
-		try {
+        BufferedReader bufRead = null;
+        InputStream is = null;
+        try {
 
-		    URL url = ResourceUtils.resolveLocation(fileName, this, context);
+            URL url = ResourceUtils.resolveLocation(fileName, this, context);
             is = url.openStream();
-		    String content = IOUtils.toString(is, "UTF-8");
+            String content = IOUtils.toString(is, "UTF-8");
 
-		    bufRead = new BufferedReader(new StringReader(content));
-			String line; // String that holds current file line
-			while ((line = bufRead.readLine()) != null) {
-				answerKey.put(line.split("[ \\t]+")[1], line);
-			}
-		}
-		catch (IOException e) {
-			throw new ResourceInitializationException(e);
-		}
-		finally {
+            bufRead = new BufferedReader(new StringReader(content));
+            int numLines = 0;
+            String line; // String that holds current file line
+            while ((line = bufRead.readLine()) != null) {
+                numLines++;
+                String wsdItemId = line.split("[ \\t]+")[1];
+                if (answerKey.put(wsdItemId, line) != null) {
+                    logger.warn(fileName + ":" + numLines
+                            + ": Duplicate answer key for " + wsdItemId);
+                }
+            }
+            logger.info(fileName + ": Read " + answerKey.size()
+                    + " unique answers");
+        }
+        catch (IOException e) {
+            throw new ResourceInitializationException(e);
+        }
+        finally {
             IOUtils.closeQuietly(bufRead);
             IOUtils.closeQuietly(is);
-		}
-	}
+        }
+    }
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    @Override
+    public void process(JCas aJCas)
+        throws AnalysisEngineProcessException
+    {
 
-		String key;
-		for (WSDItem wsdItem : JCasUtil.select(aJCas, WSDItem.class)) {
-			if ((key = answerKey.get(wsdItem.getId())) != null) {
-				WSDResult wsdResult = sensevalAnswerKeytoWSDResult(aJCas, key, wsdItem);
-				wsdResult.setSenseInventory(senseInventory);
-				wsdResult.setDisambiguationMethod(fileName);
-				wsdResult.addToIndexes();
-			}
-		}
-	}
+        String key;
+        for (WSDItem wsdItem : JCasUtil.select(aJCas, WSDItem.class)) {
+            if ((key = answerKey.get(wsdItem.getId())) != null) {
+                WSDResult wsdResult = sensevalAnswerKeytoWSDResult(aJCas, key,
+                        wsdItem);
+                wsdResult.setSenseInventory(senseInventory);
+                wsdResult.setDisambiguationMethod(fileName);
+                wsdResult.addToIndexes();
+            }
+            else {
+                logger.warn("No answer key for " + wsdItem.getId() + " in "
+                        + fileName);
+            }
+        }
+    }
 
     /**
      * convert from Senseval answer key format
      *
      * @param jCas
-     * @param answerKey A string containing a line from a Senseval answer key file
-     * @param WSDItem   The WSD item the answer key applies to, or null if this is not known.
+     * @param answerKey
+     *            A string containing a line from a Senseval answer key file
+     * @param WSDItem
+     *            The WSD item the answer key applies to, or null if this is not
+     *            known.
      */
-    public static WSDResult sensevalAnswerKeytoWSDResult(JCas jCas, String answerKey, WSDItem w)
+    public static WSDResult sensevalAnswerKeytoWSDResult(JCas jCas,
+            String answerKey, WSDItem w)
     {
-        Pattern commentPattern = Pattern.compile("^(.*)([\\t ]+!![\\t ])+(.*)$");
+        Pattern commentPattern = Pattern
+                .compile("^(.*)([\\t ]+!![\\t ])+(.*)$");
         Matcher commentMatcher = commentPattern.matcher(answerKey);
         String[] senses;
 
@@ -152,9 +207,9 @@ public class SensevalAnswerKeyReader extends JCasAnnotator_ImplBase
         // Extract lexelt/text ID and instance/head ID
         if (senses.length < 3) {
             throw new IllegalArgumentException(
-            "syntax error in Senseval answer key");
+                    "syntax error in Senseval answer key");
         }
-        //      String lexelt = senses[0];      // Not used
+        // String lexelt = senses[0]; // Not used
         String instanceId = senses[1];
 
         // Extract sense tags
@@ -169,7 +224,8 @@ public class SensevalAnswerKeyReader extends JCasAnnotator_ImplBase
                 sense.setConfidence(0.0);
             }
             else {
-                throw new IllegalArgumentException("syntax error in Senseval answer key");
+                throw new IllegalArgumentException(
+                        "syntax error in Senseval answer key");
             }
 
             sense.setId(senseParts[0]);
@@ -192,7 +248,8 @@ public class SensevalAnswerKeyReader extends JCasAnnotator_ImplBase
                 }
             }
             if (found == false) {
-                throw new IllegalArgumentException("instance/head ID '" + instanceId + "' not found");
+                throw new IllegalArgumentException("instance/head ID '"
+                        + instanceId + "' not found");
             }
         }
 
