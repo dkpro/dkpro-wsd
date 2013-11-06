3c6c50568562f2bcfb88c91c3672cefb55e7a647
tristan.miller@nothingisreal.com <tristan.miller@nothingisreal.com@6a9cb421-24ca-3a8e-1600-1ea4fc5865ef>
2013-11-06 14:18:21 +0000

    Updated test cases and documentation for version 1.0 of the WebCAGe corpus.

--- trunk/de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/WebCAGeXMLReader.java
+++ trunk/de.tudarmstadt.ukp.dkpro.wsd.io/src/main/java/de/tudarmstadt/ukp/dkpro/wsd/io/reader/WebCAGeXMLReader.java
@@ -26,6 +26,7 @@ import java.util.Iterator;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.dom4j.Document;
@@ -33,7 +34,6 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
-import org.apache.uima.fit.descriptor.ConfigurationParameter;
 
 import de.tudarmstadt.ukp.dkpro.core.api.io.ResourceCollectionReaderBase;
 import de.tudarmstadt.ukp.dkpro.core.api.metadata.type.DocumentMetaData;
@@ -44,7 +44,7 @@ import de.tudarmstadt.ukp.dkpro.wsd.type.WSDItem;
 import de.tudarmstadt.ukp.dkpro.wsd.type.WSDResult;
 
 /**
- * A collection reader for TM's XML conversion of the WebCAGe prerelease.
+ * A collection reader the WebCAGe 1.0 corpus.
  *
  * @author Tristan Miller <miller@ukp.informatik.tu-darmstadt.de>
  */
